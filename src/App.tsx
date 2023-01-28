c2b2f8285bed53810bc80e309bb05c379295ec10
Daníel Bergmann <fjallakallinn@gmail.com>
2023-01-28 16:17:06 +0000

    playing around with framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
-import React, { useState } from "react";
-import { GiLightBulb } from "react-icons/gi";
+import React, { useState } from "react"
+import { GiLightBulb } from "react-icons/gi"
+import { motion } from "framer-motion"
 
 const App: React.FC = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(false)
 
   // function to toggle darkmode
   const toggleDarkMode = (): void => {
-    setDarkMode(!darkMode);
+    setDarkMode(!darkMode)
     if (!darkMode) {
-      localStorage.setItem("theme", "dark-mode");
+      localStorage.setItem("theme", "dark-mode")
     } else {
-      localStorage.setItem("theme", "light-mode");
+      localStorage.setItem("theme", "light-mode")
     }
-  };
+  }
 
   let getTheme = (): string => {
-    let theme = localStorage.getItem("theme");
-    return theme as string;
-  };
+    let theme = localStorage.getItem("theme")
+    return theme as string
+  }
 
   return (
     <main className={getTheme()}>
-      <button onClick={toggleDarkMode}>
+      <motion.button
+        onClick={toggleDarkMode}
+        initial={{ rotate: 180, scale: 0 }}
+        animate={{ rotate: 0, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 8,
+        }}
+      >
         <GiLightBulb className={!darkMode ? "dark-mode-button" : ""} />
-      </button>
-      <div>
+      </motion.button>
+      <motion.div animate={{ y: 50 }} transition={{ delay: 0.2 }}>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
           voluptates, quod, quia, voluptate quae quidem voluptatem quibusdam
           doloremqu.
         </p>
-      </div>
+      </motion.div>
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
